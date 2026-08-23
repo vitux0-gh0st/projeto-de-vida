5414a0fdfbba8dd3b17bbe4a19c2d6b6f399ccbf
Victor...Oliveira <santos.oliveira.victor1102@escola.pr.gov.br>
2026-08-23 17:00:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -55,14 +55,4 @@ function comecaCronometro() {
     setInterval(atualizaCronometro, 1000);
 }
 
-comecaCronometro();const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-    }
-}
+comecaCronometro();
